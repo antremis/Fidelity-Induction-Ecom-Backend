f6c8464ce0f3472716518b9341848bb82a89dd60
satya-who <satyasourav1805@gmail.com>
2023-11-01 22:40:44 +0530

    4th commit

--- server.py
+++ server.py
@@ -1,6 +1,17 @@
-from flask import Flask, request, jsonify, 
+from flask import Flask, request, jsonify
 from flask_cors import CORS
+from dotenv import load_dotenv
+from config import session
+import models.CartModel as CartModel
+import models.ProductTableModel as ProductTableModel
+import models.ReviewModel as ReviewModel
+import models.TLogModel as TLogModel
+import models.TPLogsModel as TPLogsModel
+import models.UserModel as UserModel
+from sqlalchemy.orm import sessionmaker
+from ProductTableModel import displayAllProducts, addProduct
 
+load_dotenv()
 app = Flask(__name__)
 CORS(app)
 
@@ -8,9 +19,54 @@ CORS(app)
 def test():
     # Template function
     if request.method == "GET":
+        print(session)
+        TPLogsModel.insert(session, 1, 2)
         return jsonify({"mssg": "success"})
     elif request.method == "POST":
         return jsonify({"mmsg":"success", "data": [1, 2, 3, 4, 5]})
+
+@app.route("/api/product", methods=["GET", "POST"])
+def product():
+    if request.method == "GET":
+        # Get all rows from products table using sqlalchemy functions
+        all_products = displayAllProducts(session)
+        products_list = []
+        for product in all_products:
+            products_list.append({
+                "p_id": product.p_id,
+                "name": product.name,
+                "cost": product.cost,
+                "tag": product.tag,
+                "img": product.img,
+                "des": product.des,
+                "s_id": product.s_id
+            })
+        return jsonify({"products": products_list})
+    elif request.method == "POST":
+        # Get create/insert row using sqlalchemy functions
+        data = request.get_json()    #parse this JSON data and convert it into a Python dictionary
+        p_id = data.get('p_id')
+        name = data.get('name')
+        cost = data.get('cost')
+        tag = data.get('tag')
+        img = data.get('img')
+        des = data.get('des')
+        s_id = data.get('s_id')
+        new_id = addProduct(session, p_id, name, cost, tag, img, des, s_id)
+        return jsonify({"message": "Product added successfully", "p_id": new_id})
+    
+@app.route("/api/products/:id",methods=["GET","PATCH","DELETE"])
+def productById():
+    if request.method == "GET":
+        # Get 1 row from products table where products.id = id using sqlalchemy functions
+        return jsonify({"mssg": "success"})
+    elif request.method == "PATCH":
+        # Update 1 row in products table where products.id = id using sqlalchemy functions
+        return jsonify({"mssg":"success", "data": [1, 2, 3, 4, 5]})
+    elif request.method == "DELETE":
+        # Delete 1 row from products table where products.id = id using sqlalchemy functions
+        return jsonify({"mssg": "success"})
+
     
 if __name__ == "__main__":
     app.run()
