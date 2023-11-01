f92148f5e517a39ebf49623c79afbebd3eae8790
Radhika Sinhal <radhikasinhal@Radhikas-MacBook-Air.local>
2023-11-01 15:15:29 +0530

    2nd commit of R

--- models/CartModel.py
+++ models/CartModel.py
@@ -1,9 +1,12 @@
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
+from dotenv import load_dotenv
+import os
 
-DB_URL = 'mysql+pymysql://root:root@localhost/fidelity'
-DB_ENGINE = create_engine(DB_URL)
+load_dotenv()
+#DB_URL = 'mysql+pymysql://root:root@localhost/fidelity'
+DB_ENGINE = create_engine(os.getenv("DB_URL"))
 Base = declarative_base()
 
 class CartItem(Base):
@@ -15,11 +18,11 @@ class CartItem(Base):
     # CUSTOMER = relationship('Customer')
     # PRODUCT = relationship('Product')
 
-Base.metadata.create_all(create_engine(DB_ENGINE))
-Session = sessionmaker(bind=create_engine(DB_ENGINE))
-session = Session()
+Base.metadata.create_all(DB_ENGINE)
+# Session = sessionmaker(bind=create_engine(DB_ENGINE))
+# session = Session()
 
-def addToCart():
+def addToCart(session):
     u_id = int(input("Enter u_id: "))
     p_id = int(input("Enter p_id: "))
 
@@ -33,17 +36,18 @@ def addToCart():
         cart_item = CartItem(u_id=u_id, p_id=p_id, qty=quantity)
         session.add(cart_item)
         session.commit()
-        print("Product added to the cart successfully.")
-    else:
-        print("Quantity must be greater than 0.")
+        return cart_item
+    #     print("Product added to the cart successfully.")
+    # else:
+    #     print("Quantity must be greater than 0.")
     # else:
     #     print("CUSTOMER or PRODUCT not found.")
 
-def getCart(u_id):
+def getCart(session, u_id):
     cart = session.query(CartItem).filter_by(u_id=u_id).all()
     return cart
 
-def removeFromCart():
+def removeFromCart(session):
     customer_id = int(input("Enter CUSTOMER_ID: "))
     product_id = int(input("Enter PRODUCT_ID: "))
 
@@ -53,32 +57,33 @@ def removeFromCart():
 
     # if CUSTOMER and PRODUCT:
     cart_item = session.query(CartItem).filter_by(u_id=customer_id, p_id=product_id).first()
-    if cart_item:
-        session.delete(cart_item)
-        session.commit()
-        print("Product removed from the cart successfully.")
-    else:
-        print("Product not found in the CUSTOMER's cart.")
+    # if cart_item:
+    session.delete(cart_item)
+    session.commit()
+    return (cart_item)
+        # print("Product removed from the cart successfully.")
+    # else:
+    #     print("Product not found in the CUSTOMER's cart.")
     # else:
     #     print("CUSTOMER or PRODUCT not found.")
 
-while True:
-    print("1. Add Product to Cart")
-    print("2. Remove Product from Cart")
-    print("3. Get Cart Using U_ID")
-    print("4. Exit")
+# while True:
+#     print("1. Add Product to Cart")
+#     print("2. Remove Product from Cart")
+#     print("3. Get Cart Using U_ID")
+#     print("4. Exit")
 
-    choice = input("Enter your choice (1/2/3/4): ")
+#     choice = input("Enter your choice (1/2/3/4): ")
 
-    if choice == "1":
-        addToCart()
-    elif choice == "2":
-        removeFromCart()
-    elif choice == "3":
-        getCart(int(input()))
-    elif choice == "4":
-        break
-    else:
-        print("Invalid choice. Please enter 1, 2, or 3.")
+#     if choice == "1":
+#         addToCart()
+#     elif choice == "2":
+#         removeFromCart()
+#     elif choice == "3":
+#         getCart(int(input()))
+#     elif choice == "4":
+#         break
+#     else:
+#         print("Invalid choice. Please enter 1, 2, or 3.")
 
-session.close()
+# session.close()
