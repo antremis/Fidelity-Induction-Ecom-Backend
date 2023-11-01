8256b783ac5b63816268f2b7cdc38ae78aa402e3
arshita23 <arshitamadan@gmail.com>
2023-11-01 15:25:35 +0530

    Updated commit

--- models/TPLogsModel.py
+++ models/TPLogsModel.py
@@ -1,16 +1,15 @@
 from sqlalchemy import create_engine, Column, String, Integer, CHAR
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 import uuid
+import os
 
+# DB_URL="mysql+pymysql://root:23Sona81*#@localhost/E_Commerce"
 
-DB_URL="mysql+pymysql://root:23Sona81*#@localhost/E_Commerce"
-
-engine = create_engine(DB_URL, echo=True)
 
 Base = declarative_base()  
-Session = sessionmaker(bind=engine)
-session = Session()
+
 
 class TransactionProduct(Base):  #TransactionProduct
     __tablename__ = "T_P_Logs"
@@ -20,31 +19,31 @@ class TransactionProduct(Base):  #TransactionProduct
     quantity = Column("quantity", Integer, nullable = False)
     __mapper_args__={"primary_key": [t_id, p_id]}
 
-Base.metadata.create_all(engine)
 
 
 #### Query to add new data into table
 
 
-def insert(p_id, quantity):
+def insert(session, p_id, quantity):
+
     t_id = str(uuid.uuid4())
     new_entry = TransactionProduct(t_id = t_id, p_id = p_id, quantity = quantity)
     session.add(new_entry)
     session.commit()
-    #session.close()
 
-insert(1234,5)
-insert(123567,8)
-insert(5678,9)
-insert(5674,2)
-insert(6785,3)
-insert(4563,9)
+
+# insert(1234,5)
+# insert(123567,8)
+# insert(5678,9)
+# insert(5674,2)
+# insert(6785,3)
+# insert(4563,9)
 
 
 #### Insert values into a single T_id
 
 
-def generateRandomTidAndInsert(pid_quantity_list):  #generateRandomTidAndInsert
+def generateRandomTidAndInsert(session, pid_quantity_list):  #generateRandomTidAndInsert
     t_id = str(uuid.uuid4())
     for p_id, quantity in pid_quantity_list:
         new_log = TransactionProduct(t_id = t_id, p_id = p_id, quantity = quantity)
@@ -52,24 +51,24 @@ def generateRandomTidAndInsert(pid_quantity_list):  #generateRandomTidAndInsert
     session.commit()
     return t_id
 
-id = generateRandomTidAndInsert([(1,10), (2,15), (3,5)])
+# id = generateRandomTidAndInsert([(1,10), (2,15), (3,5)])
 
 
 #### Query to fetch P_id and Quantity when given T_id
 
 
-def getPidQuantityForTid(t_id):
+def getPidQuantityForTid(session, t_id):
     results = session.query(TransactionProduct.p_id, TransactionProduct.quantity).filter(TransactionProduct.t_id == t_id).all()
     return results
 
-result = getPidQuantityForTid("6cac23b8-8d68-4815-94ab-e14a906a3ed8")
-print("hii: ",result)
+# result = getPidQuantityForTid("6cac23b8-8d68-4815-94ab-e14a906a3ed8")
+# print("hii: ",result)
 
 
 ##### Query to fetch Quantity when given T_id and P_id
 
 
-def fetchQuantity(product_id_to_query, transaction_id_to_query):
+def fetchQuantity(session, product_id_to_query, transaction_id_to_query):
     quant = session.query(TransactionProduct.quantity).filter(TransactionProduct.p_id == product_id_to_query, 
                                                               TransactionProduct.t_id == transaction_id_to_query).scalar()
     return quant
@@ -86,7 +85,7 @@ def fetchQuantity(product_id_to_query, transaction_id_to_query):
 # #### Define function to delete T_id
 
 
-def deleteTransaction(transaction_id_to_delete):
+def deleteTransaction(session, transaction_id_to_delete):
     session.query(TransactionProduct).filter_by(t_id=transaction_id_to_delete).delete()
     session.commit()
 
@@ -99,3 +98,40 @@ def deleteTransaction(transaction_id_to_delete):
 # deleted_transaction = session.query(TransactionProduct).filter_by(t_id=transaction_id_to_delete).first()
 # if not deleted_transaction:
 #     print(f"Transaction ID {transaction_id_to_delete} not found.")
+
+if __name__ == "__main__":
+    load_dotenv()
+    engine = create_engine(os.getenv("DB_URL"))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    insert(session,1234,5)
+    insert(session,123567,8)
+    insert(session,5678,9)
+    insert(session,5674,2)
+    insert(session,6785,3)
+    insert(session,4563,9)
+
+    id = generateRandomTidAndInsert(session,[(1,10), (2,15), (3,5)])
+
+    result = getPidQuantityForTid(session,"6cac23b8-8d68-4815-94ab-e14a906a3ed8")
+    print("hii: ",result)
+
+    product_id_to_query = 1234
+    transaction_id_to_query = "d263ae6d-d428-4bd9-82dc-7c298ef6ef6a"
+    result = fetchQuantity(session,product_id_to_query, transaction_id_to_query)
+    if result is not None:
+        print(f"Quantity for Product ID '{product_id_to_query}' in Transaction ID '{transaction_id_to_query}': {result}")
+    else:
+        print("Product ID and Transaction ID not found.")
+
+    #### Call function with T_id to be deleted 
+    transaction_id_to_delete = "d263ae6d-d428-4bd9-82dc-7c298ef6ef6a"
+    deleteTransaction(session,transaction_id_to_delete)
+    print(f"Transaction ID {transaction_id_to_delete} has been deleted.")
+
+    #### To Verify if T_id has been deleted or not
+    deleted_transaction = session.query(TransactionProduct).filter_by(t_id=transaction_id_to_delete).first()
+    if not deleted_transaction:
+        print(f"Transaction ID {transaction_id_to_delete} not found.")
