4687a91e3fd75eb93f4f50d032e8d7e1f7bd6eab
avinasharjguru1201 <avinashrajguru1201@gmail.com>
2023-11-02 10:22:44 +0530

    3rd commit

--- server.py
+++ server.py
@@ -1,5 +1,10 @@
-from flask import Flask, request, jsonify, 
+from flask import Flask, request, jsonify
 from flask_cors import CORS
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
 
 app = Flask(__name__)
 CORS(app)
@@ -11,6 +16,25 @@ def test():
         return jsonify({"mssg": "success"})
     elif request.method == "POST":
         return jsonify({"mmsg":"success", "data": [1, 2, 3, 4, 5]})
+    
+@app.route("/api/review",methods=["GET","POST"])
+def review():
+    if request.method == "GET":
+        return jsonify({"mssg": "success"})
+    elif request.method == "POST":
+        return jsonify({"mmsg":"success", "data": [1, 2, 3, 4, 5]})
+
+@app.route("/api/review/:id",methods=["GET","PATCH","DELETE"])
+def reviewById():
+    if request.method == "GET":
+        return jsonify({"mssg": "success"})
+    elif request.method == "DELETE":
+        return jsonify({"mmsg":"success", "data": [1, 2, 3, 4, 5]})
+    elif request.method == "PATCH":
+        return jsonify({"mmsg":"success", "data": [1, 2, 3, 4, 5]})
+    
+
+
     
 if __name__ == "__main__":
     app.run()
