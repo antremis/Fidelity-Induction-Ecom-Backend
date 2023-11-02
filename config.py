98e3d4c6e77ee020eabc98888962e07ef244f056
antremis <akash7890123@hmail.com>
2023-11-02 11:08:22 +0530

    integrated backend and db

--- config.py
+++ config.py
@@ -1,2 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
-
+Base = declarative_base()
+engine = create_engine(os.getenv("DB_URL"))
+Session = sessionmaker(bind=engine)
+session = Session()
